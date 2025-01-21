915a86a28f0c965b82be825a168fab73ae9b8d1d
udit <uditmanerikar@gmail.com>
2025-01-21 22:05:54 +0530

    New Code Committed

---
+++ src/Ex_01_JavaBasics/Lab_008_Booleanvariables.java
@@ -0,0 +1,7 @@
+package Ex_01_JavaBasics;
+
+public class Lab_008_Booleanvariables {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ src/Lab_007_Variables.java
@@ -0,0 +1,19 @@
+public class Lab_007_Variables {
+    public static void main(String[] args) {
+        //variables used to store a values which could be changed like
+        // glass which can store coffe or tea
+        System.out.println();
+        int a=10;
+        /*int is primitive data type which is  numeric data type
+        a is variable ,= is operator  and 10 is the value assigned.*/
+        a=25;
+         final int  b=18;
+        System.out.println(a);
+        System.out.println(b);
+       /* int enum=28; //this is invalid jvm will be offended
+        System.out.println(enum);*/
+
+
+
+    }
+}
